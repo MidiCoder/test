ab6727dfe70a1f128732509cac48673a3b6012d6
# MidiCoder # <26660794+MidiCoder@users.noreply.github.com>
2018-11-30 11:09:53 +0000

    Update createfolders.ps1

--- createfolders.ps1
+++ createfolders.ps1
@@ -1,4 +1,6 @@
 #This basic function creates multiple directories in the specified path..
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
 Set-Location -path "C:\"
 1..5 | ForEach-Object {New-Item -ItemType Directory .\PC"$_"}
 
